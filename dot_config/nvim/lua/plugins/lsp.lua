4764def03b22418d7b24b2d96e22035066fb7877
Roman Akash Aruldoss <aruroman@amazon.com>
2023-11-04 20:34:22 +0000

    nvim config

---
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,35 @@
+return {
+  "neovim/nvim-lspconfig",
+  init = function()
+    function bemol()
+      local bemol_dir = vim.fs.find({ '.bemol' }, { upward = true, type = 'directory'})[1]
+      local ws_folders_lsp = {}
+      if bemol_dir then
+        local file = io.open(bemol_dir .. '/ws_root_folders', 'r')
+        if file then
+
+        for line in file:lines() do
+          table.insert(ws_folders_lsp, line)
+        end
+        file:close()
+        end
+      end
+
+      for _, line in ipairs(ws_folders_lsp) do
+        vim.lsp.buf.add_workspace_folder(line)
+      end
+
+      end
+    bemol()
+  end,
+  dependencies = {
+    {
+        "SmiteshP/nvim-navbuddy",
+        dependencies = {
+            "SmiteshP/nvim-navic",
+            "MunifTanjim/nui.nvim"
+        },
+        opts = { lsp = { auto_attach = true } }
+    }
+  },
+}
---
+++ dot_config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  cmd = "Neotree",
+  config = function ()
+    require("neo-tree").setup {
+      window = {
+        position = "float"
+      }
+    }
+  end
+}
